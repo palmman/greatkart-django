d524425755399276d5c9ce2ab19720b037e22d70
Worawit Boonsorn <palm454555@hotmail.com>
2021-04-25 16:44:01 +0700

    verify and resetpassword

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,14 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = 'worawit.boonsorn@gmail.com'
+EMAIL_HOST_PASSWORD = 'palm12345'
+EMAIL_USE_TLS = True
